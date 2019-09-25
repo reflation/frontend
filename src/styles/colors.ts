94846eb5661b9e1a6542a6b44a8772f8b0a1a5b1
Muhun Kim <iam@muhun.kim>
2019-09-25 23:01:16 +0900

    style: create default theme
    side effect: delete file related to create-react-app

---
+++ src/styles/colors.ts
@@ -0,0 +1,23 @@
+export const primary = 'rgba(2, 126, 204, 0.85)'
+export const secondary = 'rgba(74, 144, 226, 0.85)'
+export const white = 'rgba(255, 255, 255, 0.85)'
+export const black = 'rgba(0, 0, 0, 0.85)'
+export const darken_normal = 'rgba(0, 0, 0, 0.65)'
+export const darken_medium = 'rgba(0, 0, 0, 0.25'
+export const darken_lite = 'rgba(0, 0, 0, 0.09)'
+export const darken_ultra = 'rgba(0, 0, 0, 0.03)'
+export const background = '#f7f7f7'
+
+
+export const valid = {
+  outside: 'rgba(186, 231, 255, 0.65)',
+  inside: 'rgba(64, 169, 255, 0.65)',
+}
+
+export const invaild = {
+  outside: 'rgba(244, 179, 187, 0.65)',
+  inside: 'rgba(229, 27, 52, 0.65)',
+}
+
+export const green = 'rgba(82, 196, 26, 0.85)'
+export const red = '#f5222d'
