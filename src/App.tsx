7b16684c69bd645d6ab3d5026dc99023ce30e7ac
Muhun Kim <iam@muhun.kim>
2019-11-13 19:43:59 +0900

    style: delete create-react-app templates

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
 import React from 'react'
 
-import logo from './logo.svg'
-import './App.css'
-
 import Login from './routes/Login'
 import Main from './routes/Main'
 import Fetch from './routes/Fetch'
@@ -14,27 +11,22 @@ import {
 } from 'react-router-dom'
 
 const App: React.FC = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Redirect to="login" />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/main">
-            <Main />
-          </Route>
-          <Route path="/fetch">
-            <Fetch />
-          </Route>
-        </Switch>
-      </Router>
-    </header>
-  </div>
+  <Router>
+    <Switch>
+      <Route exact path="/">
+        <Redirect to="login" />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/main">
+        <Main />
+      </Route>
+      <Route path="/fetch">
+        <Fetch />
+      </Route>
+    </Switch>
+  </Router>
 )
 
 export default App
