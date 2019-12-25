35ac7f443da43c8537b7c21f4475882811611e9e
Muhun Kim <iam@muhun.kim>
2019-12-25 22:01:08 +0900

    feat: Redirect to main page when token exist

--- src/routes/Login/index.tsx
+++ src/routes/Login/index.tsx
@@ -3,8 +3,11 @@ import React, { useEffect } from 'react'
 import View from './view'
 import { useDispatch, useSelector } from 'react-redux'
 import { selector, setPending } from '../../store/status'
+import { ifHasTokenRedirect } from '../../utils'
 
 export default () => {
+  ifHasTokenRedirect()
+
   const { result } = useSelector(selector)
   const dispatch = useDispatch()
   useEffect(() => {
